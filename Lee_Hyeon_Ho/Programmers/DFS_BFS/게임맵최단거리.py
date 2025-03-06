65a0bb9da9036612e18724629cb6d4a557130775
이현호 <dra2sun7@gmail.com>
2025-03-06 18:02:31 +0900

    게임 맵 최단거리 / Level 2 / https://school.programmers.co.kr/learn/courses/30/lessons/1844

--- Lee_Hyeon_Ho/Programmers/DFS_BFS/게임맵최단거리.py
+++ Lee_Hyeon_Ho/Programmers/DFS_BFS/게임맵최단거리.py
@@ -1,42 +1,35 @@
-def dfs(maps, x, y, cnt, dist):
-    global n, m, mcnt
-    if x == n and y == m:
-        if dist[x][y] > cnt:
-            dist[x][y] = cnt
-        return
-            
-    print("dist 형태")
-    print("#################")
-    for i in dist:
-        print(i)    
-    print("=================")
-    if dist[x][y] >= cnt:
-        print("maps 형태")
-        print("=================")
-        for i in (maps):
-            print(i)
-        print("=================\n\n\n")
-        dist[x][y] = cnt
-        maps[x][y] = 0
-        if x > 0 and maps[x-1][y] != 0:
-            dfs(maps, x-1, y, cnt+1, dist)
-        if y > 0 and maps[x][y-1] != 0:
-            dfs(maps, x, y-1, cnt+1, dist)
-        if x < n and maps[x+1][y] != 0:
-            dfs(maps, x+1, y, cnt+1, dist)
-        if y < m and maps[x][y+1] != 0:
-            dfs(maps, x, y+1, cnt+1, dist)
-        maps[x][y] = 1
-        
+from collections import deque
+
 def solution(maps):
-    global n, m, mcnt
     n = len(maps) - 1
     m = len(maps[0]) - 1
-    mcnt = (n+1)*(m+1)
-    if maps[n-1][m] == 0 and maps[n][m-1] == 0:
-        return -1
-    dist = [[m*n for _ in range(m+1)] for _ in range(n+1)]
-    dfs(maps, 0, 0, 1, dist)
-    return dist[n][m]
+    mcnt = -1
+    
+    queue = deque([(0,0,1)])
 
-print(solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]]))
+    while queue:
+        x, y, cnt = queue.popleft()
+        
+
+        if x == n and y == m:
+            if mcnt > cnt or mcnt == -1:
+                return cnt
+                
+        if x-1 != -1 and maps[x-1][y] == 1:
+            maps[x-1][y] = 0
+            tmp = (x-1, y, cnt+1)
+            queue.append(tmp)
+        if x != n and maps[x+1][y] == 1:
+            maps[x+1][y] = 0
+            tmp = (x+1, y, cnt+1)
+            queue.append(tmp)
+        if y-1 != -1 and maps[x][y-1] == 1:
+            maps[x][y-1] = 0
+            tmp = (x, y-1, cnt+1)
+            queue.append(tmp)
+        if y != m and maps[x][y+1] == 1:
+            maps[x][y+1] = 0
+            tmp = (x, y+1, cnt+1)
+            queue.append(tmp)
+    
+    return mcnt
