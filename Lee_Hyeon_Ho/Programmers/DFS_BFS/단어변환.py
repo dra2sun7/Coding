77b2eb5ae3f72099a01bd78b3d5f810247b02b04
이현호 <dra2sun7@gmail.com>
2025-03-09 16:12:56 +0900

    단어 변환 / Level 3 / https://school.programmers.co.kr/learn/courses/30/lessons/43163

--- Lee_Hyeon_Ho/Programmers/DFS_BFS/단어변환.py
+++ Lee_Hyeon_Ho/Programmers/DFS_BFS/단어변환.py
@@ -12,7 +12,7 @@ def check_word(begin, words, queue, cnt):
         else:
             k += 1
 
-def solution(begin, target, words):
+def solution1(begin, target, words):
     global n
     queue = deque()
     n = len(begin)    
@@ -29,5 +29,35 @@ def solution(begin, target, words):
             return cnt
         
         check_word(begin, words, queue, cnt+1)
+    
+    return 0
+
+
+from collections import deque
+def check_word(begin, visited, words, queue, cnt):
+    global n
+    k = 0
+    for x in words:
+        if x not in visited:
+            if sum([w1 != w2 for w1, w2 in zip(begin, x)]) == 1:
+                queue.append((x, cnt))
+                visited.add(x)
+        
+def solution2(begin, target, words):
+    global n
+    queue = deque([(begin, 0)])
+    n = len(begin)    
+    visited = set()
+
+    if target not in words:
+        return 0
+    
+    while queue:
+        begin, cnt = queue.popleft()
+        
+        if begin == target:
+            return cnt
+        
+        check_word(begin, visited, words, queue, cnt+1)
     
     return 0
