9cd553f62bcf3a6401d492b982a4e9c0c0ebefbc
ihyeonho <dra2sun7@gmail.com>
2025-03-19 13:05:23 +0900

    퍼즐조각채우기 / Level 3 / 중간 단계 (어려움..)

--- Lee_Hyeon_Ho/Programmers/DFS_BFS/퍼즐조각채우기.py
+++ Lee_Hyeon_Ho/Programmers/DFS_BFS/퍼즐조각채우기.py
@@ -48,46 +48,59 @@ def rotate(puzzle):
             elif xm == y and ym > -x:
                 ym = -x
         
-    # print(f"tmp : {tmp}, xm : {xm}, ym : {ym}")
     for x, y in tmp:
         arr.append((x-xm, y-ym))
             
     return arr
 
-def dfs(blank, puzzle, cnt):
-    global mcnt
-    
-    # for b in blank:
-        
-    
-    
+def can_fit(blank, puzzle):
+    b_set = set(blank)
+    for _ in range(4):  # 4번 회전하면서 확인
+        p_set = set(puzzle)
+        if p_set.issubset(b_set):
+            return True
+        puzzle = rotate(puzzle)
+    return False
+
+
+def backtrack(used, count):
+    global mcnt, blank, puzzle
+
+    # 최댓값 업데이트
+    mcnt = max(mcnt, count)
+
+    for i in range(len(blank)):
+        for j in range(len(puzzle)):
+            if used[j]:  # 이미 사용한 퍼즐이면 건너뜀
+                continue
+            if can_fit(blank[i], puzzle[j]):  # 퍼즐이 빈칸에 맞으면
+                used[j] = True
+                backtrack(used, count + len(puzzle[j]))  # 퍼즐을 놓고 다음 단계로
+                used[j] = False  # 원래 상태로 되돌리기 (백트래킹)
 
 def solution(game_board, table):
-    global mcnt, n
+    global mcnt, blank, puzzle, n
     mcnt = 0
     n = len(game_board)
     blank = []
     puzzle = []
     
     board = [[game_board[i][j] for j in range(n)] for i in range(n)]
+    
     for i in range(n):
         for j in range(n):
-            if game_board[i][j] == 0:
+            if board[i][j] == 0:
                 blank.append(make_blank(board, deque([(i, j)]), i, j))
     
     board = [[table[i][j] for j in range(n)] for i in range(n)]
     for i in range(n):
         for j in range(n):
-            if table[i][j] == 1:
+            if board[i][j] == 1:
                 puzzle.append(make_puzzle(board, deque([(i, j)]), i, j))
 
-# 90도 회전 : (x , y) -> (y, -x)
-
-    for x in game_board:
-        print(x)
-    print("===============")
-    for x in table:
-        print(x)
+    used = [False] * len(puzzle)  # 퍼즐 사용 여부
     
+    backtrack(used, 0)
+
     
     return 0
