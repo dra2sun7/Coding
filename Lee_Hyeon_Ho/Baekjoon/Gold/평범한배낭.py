b09cd6985178314b2a7ee0f7d6460d707f4b8465
ihyeonho <dra2sun7@gmail.com>
2025-04-07 19:38:31 +0900

    0-1 Knapsack / Gold 5 / https://www.acmicpc.net/problem/12865

--- Lee_Hyeon_Ho/Baekjoon/Gold/평범한배낭.py
+++ Lee_Hyeon_Ho/Baekjoon/Gold/평범한배낭.py
@@ -1,16 +1,16 @@
 N,K = map(int, input().split(" "))
-dp = [0] * (K+1)
-arr = []
+dp = [[0  for _ in range(K+1)] for _ in range(N+1)]
+arr = [[0, 0]]
 for _ in range(N):
     x, y = map(int, input().split(" "))
     arr.append([x, y])
 
 # 중복 불가 문제
-arr.sort(reverse=True)
-for i in range(N):
-    for w in range(K,arr[i][0]-1, -1):
-        dp[w] = max(dp[w], dp[w-arr[i][0]] + arr[i][1])
-print(max(dp))
+# arr.sort(reverse=True)
+# for i in range(N):
+#     for w in range(K,arr[i][0]-1, -1):
+#         dp[w] = max(dp[w], dp[w-arr[i][0]] + arr[i][1])
+# print(max(dp))
 
 
 # 중복 가능 문제
@@ -21,3 +21,20 @@ print(max(dp))
 #         dp[w] = max(dp[w], dp[w-arr[i][0]] + arr[i][1])
 
 # print(max(dp))
+
+for i in range(1, N+1):
+    weight, value = arr[i]
+    
+    for w in range(K+1):
+        if w < weight:
+            dp[i][w] = dp[i-1][w]
+        else:
+            dp[i][w] = max(dp[i-1][w], dp[i-1][w-weight] + value)
+
+answer = 0
+for x in dp:
+    k = max(x) 
+    if k > answer:
+        answer = k
+        
+print(answer)
