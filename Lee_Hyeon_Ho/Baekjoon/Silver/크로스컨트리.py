7504a9938189c7e442fa095d555bc2f6c46f165e
ihyeonho <dra2sun7@gmail.com>
2025-04-24 18:04:50 +0900

    크로스 컨트리 / Silver 3 / https://www.acmicpc.net/problem/9017

---
+++ Lee_Hyeon_Ho/Baekjoon/Silver/크로스컨트리.py
@@ -0,0 +1,53 @@
+import sys
+
+dic = dict()
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    dic = dict()
+    board = dict()
+    cnt = dict()
+    idx = dict()
+    score = 0
+    N = int(sys.stdin.readline())
+    arr = sys.stdin.readline().strip().split(" ")
+    
+    for x in arr:
+        if x not in dic:
+            dic[x] = 1
+            board[x] = 0
+            cnt[x] = 0
+        else:
+            dic[x] += 1
+    
+    for x in arr:
+        if dic[x] != 6:
+            continue
+
+        else:
+            score += 1
+            
+            if cnt[x] == 4:
+                cnt[x] += 1
+                idx[x] = score    
+            elif cnt[x] == 5:
+                continue
+            else:
+                board[x] += score
+                cnt[x] += 1
+    
+    answer = 0
+    answer_size = N*N
+    for x in board:
+        if dic[x] != 6:
+            continue
+        
+        if board[x] < answer_size:
+            answer_size = board[x]
+            answer = x
+        elif board[x] == answer_size:
+            if idx[x] < idx[answer]:
+                answer = x
+    print(answer)
+    print(answer_size)
+        
