14941f55253c4ea266c34fee9aeaa4fc91f47705
ihyeonho <dra2sun7@gmail.com>
2025-04-30 17:46:06 +0900

    단지 번호 붙이기 / Silver 1 / https://www.acmicpc.net/problem/2667

--- Lee_Hyeon_Ho/Baekjoon/Silver/단지번호붙이기.py
+++ Lee_Hyeon_Ho/Baekjoon/Silver/단지번호붙이기.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 def dfs(arr, x, y, N):
     global cnt
@@ -9,6 +10,19 @@ def dfs(arr, x, y, N):
             arr[x+a][y+b] = '0'
             cnt += 1
             dfs(arr, x+a, y+b, N)
+            
+def bfs(arr, x, y, N):
+    global cnt
+    queue = deque([(x, y)])
+    tmp = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    while queue:
+        x, y = queue.popleft()
+        for a, b in tmp:
+            if -1 < x+a < N and -1 < y+b < N and arr[x+a][y+b] == '1':
+                arr[x+a][y+b] = '0'
+                cnt += 1
+                queue.append((x+a, y+b))
     
 global cnt
 arr = []
@@ -21,15 +35,26 @@ for _ in range(N):
     arr.append(list(sys.stdin.readline().strip()))
 # print("====================")
 
+# DFS 풀이
 for x in range(N):
     for y in range(N):
         if arr[x][y] == '1':
             # print("검사 시작")
-            arr[x][y] = 0
+            arr[x][y] = '0'
             cnt = 1
             dfs(arr, x, y, N)
             answer.append(cnt)
             # print("검사 종료")
+
+# BFS 풀이
+for x in range(N):
+    for y in range(N):
+        if arr[x][y] == '1':
+            arr[x][y] = '0'
+            cnt = 1
+            bfs(arr, x, y, N)
+            answer.append(cnt)
+            
             
 answer.sort()
 print(len(answer))
